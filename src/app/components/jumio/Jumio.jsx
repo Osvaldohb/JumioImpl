cfde860200288b09fb1ff3aff00f8936cd7ff55d
Osvaldo Santillan <osvaldonuevo@gmail.com>
2025-06-25 20:39:31 -0600

    build: production build

--- src/app/components/jumio/Jumio.jsx
+++ src/app/components/jumio/Jumio.jsx
@@ -1,19 +1,31 @@
 'use client'
+
 import { useEffect, useState } from 'react'
 import { AccountCreation } from '../Api/AccountCreation'
 import { FetchToken } from '../Api/Fetch'
 
 import dynamic from 'next/dynamic'
+import { JumioAccountCreation } from '../Api/jumioAccountCreation'
 
 const JumioComponent = dynamic(() => import('./JumioComponent'), { ssr: false })
 
 export default function JumioJsx() {
-  const [token, setToken] = useState('')
+  const [sdkToken, setSdkToken] = useState('')
 
+  useEffect(() => {
+    const fetchSdkToken = async () => {
+      try {
+        const tokenData = await JumioAccountCreation()
+       setSdkToken(tokenData)
+      } catch (error) {
+        console.error('Error generating Jumio SDK token:', error)
+      }
+    }
 
-  return (
-    <div className="h-screen">
-      <JumioComponent token="eyJhbGciOiJIUzUxMiIsInppcCI6IkdaSVAifQ.H4sIAAAAAAAA_5XLOw4CMQwE0LukxpLzd-goabmB49iIhgJWAglxd5K9AcUUM3rzcfo-be7ofM1IrUScIXdwLHIeczdUTUoNQh4KKWoASsoQOyv1gSKBF99xzRVDI4W4XMpYgTrxvA0Rz5HMbOKX6T9cLmpTP7fH7X5dfT_7Yd5KR1DRBKkwAxt2IE9N-sjci7jvD7rXvvbgAAAA.6ggYHuRGPg6ztzKnCYnAwIae4wGZTvOH2QEnMTpzYggKdu2oRrB_DeHzcZokSiKmCeRGSJLSdZT_8-UVuYzm4Q" />
-    </div>
-  )
+    fetchSdkToken()
+  }, [])
+
+  if (!sdkToken) return <p>Loading Jumio...</p>
+
+  return <JumioComponent token={sdkToken} />
 }
